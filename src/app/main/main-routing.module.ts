d038c1a2302467f3ef3ea3324ad68c8da89623d2
Bang Ngo <bang.ngo@logigear.com>
2021-06-19 19:37:12 +0700

    Added Question Management Component

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {InexamComponent} from "./inexam/inexam.component";
+import {MainComponent} from "./main.component";
+import {QuestionsManagementComponent} from "./questions-management/questions-management.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        pathMatch: 'full',
+        component: HomeComponent
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      {
+        path: 'exam',
+        component: InexamComponent
+      },
+      {
+        path: 'questionsManagement',
+        component: QuestionsManagementComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,21 +1,57 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MainRoutingModule } from './main-routing.module';
+import {MainRoutingModule} from './main-routing.module';
 import {PanelMenuModule} from "primeng/panelmenu";
 import {SidebarModule} from "primeng/sidebar";
 import {MenubarModule} from "primeng/menubar";
 import {SidebarComponent} from "../layout/sidebar/sidebar.component";
+import {HomeComponent} from './home/home.component';
+import {SkeletonModule} from "primeng/skeleton";
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {MainComponent} from "./main.component";
+import {InexamComponent} from "./inexam/inexam.component";
+import {DialogModule} from "primeng/dialog";
+import {ButtonModule} from "primeng/button";
+import { QuestionsManagementComponent } from './questions-management/questions-management.component';
+import {ToolbarModule} from "primeng/toolbar";
+import {RippleModule} from "primeng/ripple";
+import {TableModule} from "primeng/table";
+import {RatingModule} from "primeng/rating";
+import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {InputNumberModule} from "primeng/inputnumber";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ConfirmationService} from "primeng/api";
+import {DialogService} from "primeng/dynamicdialog";
 
 
 @NgModule({
-  declarations: [SidebarComponent,],
-  imports: [
-    CommonModule,
-      PanelMenuModule,
-      SidebarModule,
-      MenubarModule,
-    MainRoutingModule
-  ]
+    providers: [ConfirmationService, DialogService],
+    declarations: [MainComponent, SidebarComponent, HomeComponent, InexamComponent, QuestionsManagementComponent],
+    imports: [
+        CommonModule,
+        PanelMenuModule,
+        SidebarModule,
+        MenubarModule,
+        MainRoutingModule,
+        SkeletonModule,
+        CardModule,
+        InputTextModule,
+        DialogModule,
+        ButtonModule,
+        ToolbarModule,
+        RippleModule,
+        TableModule,
+        RatingModule,
+        FormsModule,
+        DropdownModule,
+        RadioButtonModule,
+        InputNumberModule,
+        ConfirmDialogModule
+    ]
 })
-export class MainModule { }
+export class MainModule {
+}
